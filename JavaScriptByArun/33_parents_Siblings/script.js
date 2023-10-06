9da0f8897bae4ed815cc3e6f3fa552cdff4ca32f
Arunvarma1862 <arunbabu120894@gmail.com>
2023-10-06 15:04:57 +0530

    6/9

---
+++ JavaScriptByArun/33_parents_Siblings/script.js
@@ -0,0 +1,8 @@
+console.log(document.body.firstChild)
+a=document.body.firstChild
+console.log(a)
+console.log(a.parentNode);
+console.log(a.parentElement)
+// console.log(document.documentElement.parentNode);
+// console.log(document.documentElement.parentElement);
+console.log(a.firstChild.nextSibling)
