c67cf4e907e29539ccab99f0bc15fde28d59e693
Arunvarma1862 <arunbabu120894@gmail.com>
2023-11-11 12:33:37 +0530

    11/11

---
+++ JavaScript/practice.js
@@ -0,0 +1,13 @@
+const item ={
+    names:"Hello",
+    price:56,
+    indian:true,
+    char:null
+    // nnssbbu
+}
+console.log(item["names"])
+console.log(item.price)
+
+let a="Hello World"
+console.log(typeof a)
+console.log(a+15)
